75e557bcdb9d62211f19c0c4e99057410326632a
zhaoshengDS <lzsvan123@gmail.com>
2022-03-29 16:03:41 -0700

    Upload matte extraction code

---
+++ matte_extraction.py
@@ -0,0 +1,20 @@
+"""
+Extracting alpha matte from the matted image in .png format
+"""
+
+from tqdm import tqdm
+import cv2
+import os
+
+dataset = "./portrait/"
+source_folder = "target/"
+destination_folder = "matte/"
+
+image_list = os.listdir(dataset + source_folder)
+
+for i, img_name in enumerate(tqdm(image_list)):
+    image = cv2.imread(dataset + source_folder + img_name, cv2.IMREAD_UNCHANGED)  
+    alpha_matte = image[:,:,3]
+    cv2.imwrite(dataset + destination_folder + img_name, alpha_matte)
+
+print("Mission Accomplished")
